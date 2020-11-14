3a1c52f3130dd9e40f2df275963d92e0b3b45887
haran <harangovindan@gmail.com>
2020-11-14 13:51:25 +0000

    lmi: Mount and bind audio_policy_configuration in /vendor/etc/audio

--- kona.mk
+++ kona.mk
@@ -42,7 +42,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml
+     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
