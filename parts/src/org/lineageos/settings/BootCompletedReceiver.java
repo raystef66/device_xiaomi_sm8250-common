d76ef8a0bfeb8a75351d8fbf8af8b9bf44b89cd5
RealSchnedi <schnedi3@gmail.com>
2021-03-26 23:04:51 +0100

    sm8250-common: parts: Drop doze

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.os.SystemProperties;
 import android.util.Log;
 
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.fod.FodUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -37,7 +36,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         final boolean needs_fod_service = Boolean.parseBoolean(
             SystemProperties.get(FOD_PROP, "true"));
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        DozeUtils.checkDozeService(context);
         if (needs_fod_service) FodUtils.startService(context);
     }
 }
