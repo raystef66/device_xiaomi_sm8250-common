270e549373facbfd0f6e547e49c3f95ecbee6b7d
SebaUbuntu <barezzisebastiano@gmail.com>
2021-04-25 15:06:27 +0200

    sm8250-common: Build touchfeature HIDL lib from source
    Change-Id: Ib92a8af07422873dd9667ebfdbb25a4417b1abde

--- kona.mk
+++ kona.mk
@@ -191,7 +191,8 @@ PRODUCT_PACKAGES += \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
+    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
+    vendor.xiaomi.hardware.touchfeature@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     vendor/derp/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
