09a0ae4691dbc7cb54ef51b013a269f5a5b32e62
SebaUbuntu <barezzisebastiano@gmail.com>
2021-06-20 10:13:40 +0200

    sm8250-common: fod: Bump to v1.1
    Change-Id: Ib24f1bd4f1f68c1c5478a4cf01f5c43aca1ca6c3

--- kona.mk
+++ kona.mk
@@ -181,7 +181,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FODService \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_kona \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.1-service.xiaomi_kona \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
     vendor.xiaomi.hardware.touchfeature@1.0.vendor
 
