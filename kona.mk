5d4c8053ca40b17d0a385be89267523842d60af3
SebaUbuntu <barezzisebastiano@gmail.com>
2021-06-22 19:14:38 +0200

    sm8250-common: Build custom lights AIDL
    Change-Id: Id7d8c2c5c141dba8006eb6e800dcd8777e1441d6

--- kona.mk
+++ kona.mk
@@ -213,9 +213,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.kona
+    android.hardware.lights-service.xiaomi_kona
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
