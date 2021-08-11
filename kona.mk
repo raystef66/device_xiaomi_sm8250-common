6f3dc9bd60b2023940c16f3ce246cc4c8db8ec59
Dobsgw <George.w.dobson@gmail.com>
2021-08-11 18:13:30 +0000

    sm8250-common: import libnfc-nci.conf to tree

--- kona.mk
+++ kona.mk
@@ -274,6 +274,9 @@ PRODUCT_PACKAGES += \
     jcos_nq_client \
     vendor.nxp.hardware.nfc@2.0-service
 
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+
 # OMX
 PRODUCT_PACKAGES += \
     init.qti.media.sh \
